b668f92e654a9a7ee9567341294c7f96a53511fc
Ellen Su <eysu@cs.princeton.edu>
2021-11-12 14:53:50 -0500

    Initial version

--- 3FT/node.h
+++ 3FT/node.h
@@ -103,7 +103,7 @@ void* Node_updateFileContents(Node_T n, void *contents);
    Updates the node n's length field. Resets n->length to 
    new_length. 
 */
-void Node_updateFileLength(Node_T n, size_t new_length);
+void Node_updateLength(Node_T n, size_t new_length);
 
 /*
    Returns a DynArray representation of the contents of a file node, 
